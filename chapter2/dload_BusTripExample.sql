868696adb61dea2025e29cf66ad59c6fd8ce6d53
alejandro-vaisman <161719596+alejandro-vaisman@users.noreply.github.com>
2025-02-20 10:05:44 -0300

    Add files via upload

---
+++ chapter2/dload_BusTripExample.sql
@@ -0,0 +1,56 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2821
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red11\green90\blue178;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c43922\c75294;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww21520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs37\fsmilli18667 \cf2 \expnd0\expndtw0\kerning0
+CREATE TABLE BusTrip(TripId integer, Trip tgeompoint);
+\f1\fs24 \cf3 \
+\
+\
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf3 INSERT INTO BusTrip VALUES
+\f1\fs24 \
+
+\f0\fs32 (1, tgeompoint '\{[SRID=3812;POINT(651096.993815166 667028.114604598)@2020-04-21 08:37:27, 
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651080.424535144 667123.352304597)@2020-04-21 08:37:39,
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651067.607438095 667173.570340437)@2020-04-21 08:38:06,
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651052.741845233 667213.026797244)@2020-04-21 08:38:31,
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651029.676773636 667255.556944161)@2020-04-21 08:38:49, 
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651018.401101238 667271.441380755)@2020-04-21 08:39:08)\}')
+\f1\fs24 \
+\
+
+\f0\fs32 INSERT INTO BusTrip VALUES
+\f1\fs24 \
+
+\f0\fs32 (2, tgeompoint '\{[SRID=3812;POINT(651262.17004873 667119.331513367)@2020-04-21 08:38:29, 
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651201.431447782 667089.682115196)@2020-04-21 08:38:36,
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651186.853162155 667091.138189286)@2020-04-21 08:38:43,
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651181.995412783 667077.531372716)@2020-04-21 08:38:49,
+\f1\fs24 \
+
+\f0\fs32 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0 SRID=3812;POINT(651101.820139904 667041.076539663)@2020-04-21 08:38:56)\}')
+\f1\fs24 \
+
+\f0\fs32 \'a0 }
